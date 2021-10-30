149b856dc759ebe635b8c306c6a061b48214265e
unknown <aliashraftamim@gmail.com>
2021-10-31 01:43:09 +0600

    ADD Day 12 class - 8

--- js.js
+++ js.js
@@ -1,24 +1,40 @@
-// Day 12 Class - 7
-var x = 5;
-var y = 10.5;
+// // Day 12 Class - 7
+// var x = 5;
+// var y = 10.5;
 
-// Number to string convert >>
-// var t = ''+x
-console.log( x + z)
+// // Number to string convert >>
+// // var t = ''+x
+// console.log( x + z)
 
-var a = 5;
-var b = "10.5";
-// string to number convert >>
-// var d = parseFloat(b);
-// var d = parseInt(b);
-// var d = +y; 
+// var a = 5;
+// var b = "10.5";
+// // string to number convert >>
+// // var d = parseFloat(b);
+// // var d = parseInt(b);
+// // var d = +y; 
 
-// Fixed number value show >>
-// x.toFixed(2);
-console.log( x + z)
+// // Fixed number value show >>
+// // x.toFixed(2);
+// console.log( x + z)
 
-var o = .2;
-var p = .1;
-var q = x + y;
-var c = z.toFixed(1);
-console.log(c);
+// var o = .2;
+// var p = .1;
+// var q = x + y;
+// var c = z.toFixed(1);
+// console.log(c);
+
+// // Day 12 Class - 8
+
+// var x = 17;
+// var y = 3;
+// console.log(x % y);
+
+// var x = 15;
+// var y = 4;
+// x++;
+// console.log(x);
+
+// var x = 15;
+// var y = 4;
+// x--;
+// console.log(x);
