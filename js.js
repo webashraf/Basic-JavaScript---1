9f559952b3fb66a411b74e30cd5afdebc5931e8f
unknown <aliashraftamim@gmail.com>
2021-10-30 20:51:35 +0600

    Day 12 Class 12 - Practices code

---
+++ js.js
@@ -0,0 +1,23 @@
+var x = 5;
+var y = 10.5;
+
+// Number to string convert >>
+// var t = ''+x
+console.log( x + z)
+
+var a = 5;
+var b = "10.5";
+// string to number convert >>
+// var d = parseFloat(b);
+// var d = parseInt(b);
+// var d = +y; 
+
+// Fixed number valu show >>
+// x.toFixed(2);
+console.log( x + z)
+
+var o = .2;
+var p = .1;
+var q = x + y;
+var c = z.toFixed(1);
+console.log(c);
