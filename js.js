3cf417ccc63cd8767e9441ef153a014a21bcb891
unknown <aliashraftamim@gmail.com>
2021-10-31 20:08:42 +0600

    Add class 12/9

--- js.js
+++ js.js
@@ -38,3 +38,33 @@
 // var y = 4;
 // x--;
 // console.log(x);
+
+// // Day 12 Class - 9
+
+// number absolute
+// 
+//যে কোন নাম্বার কে পজেটিভ করে সো করাবে যেমনঃ -5 কে Math.abs করলে 5 করে দিবে।
+// let numbr = -5;
+// let absoluteNumber = Math.abs(numbr);
+// console.log(absoluteNumber);
+
+// দশমিক নাম্বার কে পূর্ন সংখ্যায় কনভার্ট করতে Math.round(); ব্যাবহার করতে হবে। এটার কাজ হচ্ছে 5.400001 / 5.604450 মানে দশমিকের পরের সংখ্যা যদি ৫ অথবা ৫ এর বড় হয় তাহলে পূর্ন সংখার সাথে এক যোগ করে ভ্যালু দেখাবে আর যদি ছোট হয় তাহলে পূর্ন সংখ্যার সাথে এক বিয়োগ করে দিবে।।
+// let numbr = 5.6545251;
+// let absoluteNumber = Math.round(numbr);
+// console.log(absoluteNumber);
+
+// দশমিক নাম্বার কে পূর্ন সংখ্যায় কনভার্ট করতে Math.round(); ব্যাবহার করতে হবে। তবে যদি  Math.ceil(); দেয়া হয় তাহলে দশমিকের পর যদি .০০০০১ ও থাকে তাহলে সব সময় ১ যোগ করবে, তবে যদি শুধু শূন্য থাকে তাহলে যোগ করবেনা।
+// let numbr = 5.000145251;
+// let absoluteNumber = Math.ceil(numbr);
+// console.log(absoluteNumber);
+// // দশমিক নাম্বার কে পূর্ন সংখ্যায় কনভার্ট করতে Math.round(); ব্যাবহার করতে হবে। তবে যদি  Math.floor(); দেয়া হয় তাহলে সব সময় পূর্ন সংখ্যায় যা ভ্যালু আছে তাই দেখাবে।
+// let numbr = 5.00000001;
+// let absoluteNumber = Math.ceil(numbr);
+// console.log(absoluteNumber);
+
+
+// যদি এক এক বার এক সংখ্যা লঠারির মতন ভ্যালু সো করাতে Math.random()*100; ব্যাবহার করতে হবে।
+// let numbr = 5.00000001;
+let numbr = Math.random()*100;
+let absoluteNumber = Math.round(numbr);
+console.log(absoluteNumber);
